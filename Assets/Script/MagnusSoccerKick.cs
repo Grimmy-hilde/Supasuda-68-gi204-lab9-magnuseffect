9f4ba623215739cd4f62d559ad5b5b3d7a9bc678
Grimmy-hilde <gamsriwan@gmail.com>
2026-03-12 11:05:31 +0700

    finish

--- Assets/Script/MagnusSoccerKick.cs
+++ Assets/Script/MagnusSoccerKick.cs
@@ -21,8 +21,9 @@ public class MagnusSoccerKick : MonoBehaviour
     {
         if(Keyboard.current.spaceKey.wasReleasedThisFrame && !isShot)
         {
-            rb.AddForce(Vector3.forward * kickForce, ForceMode.Impulse);
             rb.AddTorque(Vector3.up * spinAmount);
+            rb.AddForce(Vector3.forward * kickForce, ForceMode.Impulse);
+            
 
             isShot = true ;
         }
